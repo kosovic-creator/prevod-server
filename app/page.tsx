6beed34928c442f7389a6beec9bb47b0d39c274f
kosovic-creator <drasko.kosovic@icloud.com>
2025-12-04 18:40:12 +0100

    innit2

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-import { cookies, headers } from 'next/headers';
+// import { cookies, headers } from 'next/headers';
 
 
-async function getLocale() {
-  // Pokušaj da uzmeš jezik iz cookie-a ili headera
-  const cookieStore = await cookies();
-  const cookieLocale = cookieStore.get('NEXT_LOCALE')?.value;
-  const headerStore = await headers();
-  const acceptLang = headerStore.get('accept-language');
-  if (cookieLocale) return cookieLocale;
-  if (acceptLang) {
-    if (acceptLang.startsWith('sr')) return 'sr';
-    if (acceptLang.startsWith('en')) return 'en';
-  }
-  return 'sr';
-}
+// async function getLocale() {
+//   // Pokušaj da uzmeš jezik iz cookie-a ili headera
+//   const cookieStore = await cookies();
+//   const cookieLocale = cookieStore.get('NEXT_LOCALE')?.value;
+//   const headerStore = await headers();
+//   const acceptLang = headerStore.get('accept-language');
+//   if (cookieLocale) return cookieLocale;
+//   if (acceptLang) {
+//     if (acceptLang.startsWith('sr')) return 'sr';
+//     if (acceptLang.startsWith('en')) return 'en';
+//   }
+//   return 'sr';
+// }
 
 export default function Home() {
   return (
