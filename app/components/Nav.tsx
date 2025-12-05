a650674b2a130ef984a50ddc427bee1a16abbd92
kosovic-creator <drasko.kosovic@icloud.com>
2025-12-05 21:22:22 +0100

    ama

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -10,9 +10,6 @@ export default function Nav() {
   const localeMatch = pathname.match(/^\/(en|sr)(\/|$)/);
   const currentLocale = localeMatch ? localeMatch[1] : 'en';
 
-  // Helper: remove locale segment from pathname
-
-
 function getBasePath(pathname: string): string {
     const parts: string[] = pathname.split('/');
     // Ako je prvi segment locale, ukloni ga
@@ -21,15 +18,12 @@ function getBasePath(pathname: string): string {
     }
     return pathname;
 }
-
   const basePath = getBasePath(pathname) || '/';
-
   return (
     <nav style={{ marginBottom: '2rem' }}>
       <Link locale={currentLocale} href="/" style={{ marginRight: '1rem' }}>{t('home')}</Link>
       <Link locale={currentLocale} href="/prevod" style={{ marginRight: '1rem' }}>{'prevod'}</Link>
       <Link locale={currentLocale} href="/prevod2" style={{ marginRight: '1rem' }}>{'prevod2'}</Link>
-      {/* <Link locale={currentLocale} href="/test" style={{ marginRight: '1rem' }}>{'test (dynamic)'}</Link> */}
       <Link locale="sr" href={basePath} style={{ marginRight: '1rem' }}>{t('sr')}</Link>
       <Link locale="en" href={basePath}>{t('en')}</Link>
     </nav>
